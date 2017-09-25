730ff260169e46855c36636a14e5de4cc0a9e314
Artenuvielle <renem1@gmx.net>
2017-09-25 17:30:50 +0200

    added vr blueprints

--- Source/TheGrid/TheGrid.Build.cs
+++ Source/TheGrid/TheGrid.Build.cs
@@ -10,7 +10,7 @@ public class TheGrid : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "libprotobuf", "enet" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "libprotobuf", "enet", "HeadMountedDisplay", "SteamVR" });
 
         // Uncomment if you are using Slate UI
         // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
